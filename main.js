81e81919d54421f2161d5cdf5c69e28942ed92d9
csing-95 <corrine.sing@kinsmengroup.com>
2025-09-11 17:34:07 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 900,
+    height: 700,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
+
+  win.loadFile('index.html'); // make sure your timecard file is index.html
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
